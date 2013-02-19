0cb06f579947528e386f0f52beef47bd8113f0af
Ubuntu <ubuntu@ip-10-172-233-42.us-west-1.compute.internal>
2013-02-19 07:54:00 +0000

    modified controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,29 +41,26 @@ class UsersController < ApplicationController
   # POST /TESTAPI/unitTests
   def userUnitTests
     # @unit_test_output = %x(rake test TEST=test/unit/user_test.rb)
-    @unit_test_output = %x(ruby -ltest test/unit/user_test.rb)
+    @unit_test_output = %x(ruby -Itest test/unit/user_test.rb)
     # @unit_test_output = open("../../unit_test_output")
     #we need to find the line that starts with "[Number] tests"
-   # @results = ''
-   # @unit_test_output.each_line do |li|
-   #   if (li[/^[0-9]+ tests.*/])
-   #     @results = li
-    #    break
-   #   end
-   # end
-    # @unit_test_output = "hello rails!"
-   # /[0-9]+ tests/ =~ @results
-   # /[0-9]+/ =~ Regexp.last_match[0]
-   # @total_tests = Regexp.last_match[0].to_i
-   # # @total_tests = 0
-   # /[0-9]+ failures/ =~ @results
-   # /[0-9]+/ =~ Regexp.last_match[0]
-    #@failures = Regexp.last_match[0].to_i
+    @results = ''
+    @unit_test_output.each_line do |li|
+      if (li[/^[0-9]+ tests.*/])
+        @results = li
+        break
+      end
+    end
+    /[0-9]+ tests/ =~ @results
+    /[0-9]+/ =~ Regexp.last_match[0]
+    @total_tests = Regexp.last_match[0].to_i
+    # @total_tests = 0
+    /[0-9]+ failures/ =~ @results
+    /[0-9]+/ =~ Regexp.last_match[0]
+    @failures = Regexp.last_match[0].to_i
     # @failures = 0
-   
-  # render :json => { 'totalTests' => @total_tests, 'nrFailed' => @failures, 'output' => @unit_test_output }
- 
-    render :json => { 'totalTests' => 11, 'nrFailed' => 0, 'output' => @unit_test_output }
+    render :json => { 'totalTests' => @total_tests, 'nrFailed' => @failures, 'output' => @unit_test_output }
+    #render :json => { 'totalTests' => 11, 'nrFailed' => 0, 'output' => @unit_test_output }
   end
 
   # GET /users/1
